8d2b69be3da8194090ea0f631ac9fbe6f1bf63dd
Jeffrey Liu <jeffreyliu8@gmail.com>
2019-05-30 16:43:33 -0700

    viewModel scope coroutin

--- app/src/main/java/com/askjeffreyliu/githubsearch/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/askjeffreyliu/githubsearch/viewmodel/MainViewModel.kt
@@ -2,20 +2,22 @@ package com.askjeffreyliu.githubsearch.viewmodel
 
 import android.app.Application
 import android.text.TextUtils
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.askjeffreyliu.githubsearch.model.QueryResult
 import com.askjeffreyliu.githubsearch.repository.MainRepository
 import kotlinx.coroutines.launch
 
 
-class MainViewModel(application: Application) : ScopeAndroidViewModel(application) {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     private val repository = MainRepository(application)
 
     private val liveData = MutableLiveData<QueryResult>()
 
     fun search(query: String) {
-        launch(context = coroutineContext) {
+        viewModelScope.launch  {
             if (TextUtils.isEmpty(query)) {
                 liveData.value = null
             } else {
