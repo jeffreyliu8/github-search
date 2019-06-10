dca0605d57cacac013ee47f4bb70bf373965776e
Jeffrey Liu <jeffreyliu8@gmail.com>
2019-06-10 16:53:02 -0700

    dagger update

--- app/src/main/java/com/askjeffreyliu/githubsearch/dependency/WebComponent.kt
+++ app/src/main/java/com/askjeffreyliu/githubsearch/dependency/WebComponent.kt
@@ -1,7 +1,6 @@
 package com.askjeffreyliu.githubsearch.dependency
 
 
-import com.askjeffreyliu.githubsearch.repository.MainRepository
 import com.askjeffreyliu.githubsearch.viewmodel.MainViewModel
 import dagger.Component
 import javax.inject.Singleton
@@ -9,7 +8,5 @@ import javax.inject.Singleton
 @Singleton
 @Component(modules = [WebModule::class])
 interface WebComponent {
-    fun inject(repository: MainRepository)
-
     fun inject(viewModel: MainViewModel)
 }
