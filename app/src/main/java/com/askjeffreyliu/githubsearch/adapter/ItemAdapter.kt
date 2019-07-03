e2158b358b9d6c8dee87dad393dfda5eeac218f7
Jeffrey Liu <jeffreyliu8@gmail.com>
2019-07-03 14:04:33 -0700

    class arrangement

--- app/src/main/java/com/askjeffreyliu/githubsearch/adapter/ItemAdapter.kt
+++ app/src/main/java/com/askjeffreyliu/githubsearch/adapter/ItemAdapter.kt
@@ -11,7 +11,7 @@ import kotlinx.android.synthetic.main.list_item.view.*
 
 
 class ItemAdapter(private val listener: (QueryItem) -> Unit) :
-    RecyclerView.Adapter<ViewHolder>() {
+    RecyclerView.Adapter<ItemAdapter.MyViewHolder>() {
 
     private var mList: List<QueryItem>? = null
 
@@ -20,27 +20,27 @@ class ItemAdapter(private val listener: (QueryItem) -> Unit) :
         notifyDataSetChanged()
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false))
     }
 
     override fun getItemCount(): Int {
         return mList?.size ?: 0
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) =
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) =
         holder.bind(mList!![position], listener)
-}
 
-class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    fun bind(item: QueryItem, listener: (QueryItem) -> Unit) = with(itemView) {
-        nameTextView.text = item.fullName
-        descriptionTextView.text = item.description
-        starTextView.text = "★" + item.stargazersCount
-        Picasso.get().load(item.owner.avatarUrl).into(avatarImageView)
+    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(item: QueryItem, listener: (QueryItem) -> Unit) = with(itemView) {
+            nameTextView.text = item.fullName
+            descriptionTextView.text = item.description
+            starTextView.text = "★" + item.stargazersCount
+            Picasso.get().load(item.owner.avatarUrl).into(avatarImageView)
 
-        setOnClickListener {
-            listener(item)
+            setOnClickListener {
+                listener(item)
+            }
         }
     }
 }
