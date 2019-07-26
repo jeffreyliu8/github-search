bac9441c611e9d13919d3bccd97198cd64fdb619
Jeffrey Liu <jeffreyliu8@gmail.com>
2019-07-26 12:50:48 -0700

    add simple test

---
+++ app/src/androidTest/java/com/askjeffreyliu/githubsearch/MainActivityTest.kt
@@ -0,0 +1,112 @@
+package com.askjeffreyliu.githubsearch
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.hamcrest.core.IsInstanceOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest() {
+        val textInputEditText = onView(
+            allOf(
+                withId(R.id.editText),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.inputLayout),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText.perform(click())
+
+        val textInputEditText2 = onView(
+            allOf(
+                withId(R.id.editText),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.inputLayout),
+                        0
+                    ),
+                    0
+                ),
+                isDisplayed()
+            )
+        )
+        textInputEditText2.perform(replaceText("square"), closeSoftKeyboard())
+
+        val materialButton = onView(
+            allOf(
+                withId(R.id.button), withText("search"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton.perform(click())
+
+        Thread.sleep(5000)
+
+        val textView = onView(
+            allOf(
+                withId(R.id.nameTextView), withText("square/retrofit"),
+                childAtPosition(
+                    childAtPosition(
+                        IsInstanceOf.instanceOf(android.widget.FrameLayout::class.java),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("square/retrofit")))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
