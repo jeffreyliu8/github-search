ce81d99380b07ef29ecd6aa81d6c418af2340335
Jeffrey Liu <jeffreyliu8@gmail.com>
2021-01-06 15:13:06 -0800

    gradle update

--- app/src/main/java/com/askjeffreyliu/githubsearch/MainActivity.kt
+++ app/src/main/java/com/askjeffreyliu/githubsearch/MainActivity.kt
@@ -7,23 +7,26 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.askjeffreyliu.githubsearch.adapter.ItemAdapter
+import com.askjeffreyliu.githubsearch.databinding.ActivityMainBinding
 import com.askjeffreyliu.githubsearch.model.ResourceState
 import com.askjeffreyliu.githubsearch.viewmodel.MainViewModel
 import com.google.android.material.snackbar.Snackbar
 import com.orhanobut.logger.Logger
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: ItemAdapter
     private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         setupRecyclerView()
         setupViewModel()
@@ -32,37 +35,37 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupRecyclerView() {
         // Creates a vertical Layout Manager
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
 
         // Access the RecyclerView Adapter
         adapter = ItemAdapter {
             Logger.d(it.fullName)
         }
-        recyclerView.adapter = adapter
+        binding.recyclerView.adapter = adapter
     }
 
     private fun setupViewModel() {
         viewModel.liveData.observe(this, Observer { resource ->
             resource.let {
                 when (it.state) {
-                    ResourceState.LOADING -> progressBar.visibility = View.VISIBLE
+                    ResourceState.LOADING -> binding.progressBar.visibility = View.VISIBLE
                     else -> {
-                        progressBar.visibility = View.GONE
+                        binding.progressBar.visibility = View.GONE
                     }
                 }
                 it.data?.let { result ->
                     adapter.updateList(result.items)
                 }
                 it.message?.let { msg ->
-                    Snackbar.make(progressBar, msg, Snackbar.LENGTH_LONG).show()
+                    Snackbar.make(binding.progressBar, msg, Snackbar.LENGTH_LONG).show()
                 }
             }
         })
     }
 
     private fun setupUserInput() {
-        button.setOnClickListener {
-            val searchText = editText.text.toString().trim()
+        binding.button.setOnClickListener {
+            val searchText = binding.editText.text.toString().trim()
             viewModel.search(searchText)
         }
     }
--- app/src/main/java/com/askjeffreyliu/githubsearch/adapter/ItemAdapter.kt
+++ app/src/main/java/com/askjeffreyliu/githubsearch/adapter/ItemAdapter.kt
@@ -1,13 +1,11 @@
 package com.askjeffreyliu.githubsearch.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.askjeffreyliu.githubsearch.R
+import com.askjeffreyliu.githubsearch.databinding.ListItemBinding
 import com.askjeffreyliu.githubsearch.model.QueryItem
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.list_item.view.*
 
 
 class ItemAdapter(private val listener: (QueryItem) -> Unit) :
@@ -21,9 +19,9 @@ class ItemAdapter(private val listener: (QueryItem) -> Unit) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        return MyViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
-        )
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ListItemBinding.inflate(inflater, parent, false)
+        return MyViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
@@ -33,12 +31,12 @@ class ItemAdapter(private val listener: (QueryItem) -> Unit) :
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) =
         holder.bind(mList!![position], listener)
 
-    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class MyViewHolder(private val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item: QueryItem, listener: (QueryItem) -> Unit) = with(itemView) {
-            nameTextView.text = item.fullName
-            descriptionTextView.text = item.description
-            starTextView.text = "★" + item.stargazersCount
-            Picasso.get().load(item.owner.avatarUrl).into(avatarImageView)
+            binding.nameTextView.text = item.fullName
+            binding.descriptionTextView.text = item.description
+            binding.starTextView.text = "★" + item.stargazersCount
+            Picasso.get().load(item.owner.avatarUrl).into(binding.avatarImageView)
 
             setOnClickListener {
                 listener(item)
